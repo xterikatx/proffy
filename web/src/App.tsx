e7df835202cbf9b77c0769f9667879fa949880e9
erikalopes <201810099@aluno.fjn.edu.br>
2020-08-06 23:02:34 -0300

    add layout initial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './assets/styles/globals.css';
+import Landing from './pages/Landing';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+  return (    
+      <Landing />
   );
 }
 
