282bb2a1b09da1cfd501a9e8eb2f8b5a83ecc22f
erikalopes <201810099@aluno.fjn.edu.br>
2020-08-07 23:07:00 -0300

    first insertion - Let’s call it a day

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import db from "./database/connection";
+
+const routes = express.Router();
+
+routes.post("/classes", async (request, response) => {
+  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+
+  await db("users").insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  });
+
+  return response.json({});
+});
+
+export default routes;
