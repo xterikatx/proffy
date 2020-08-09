ef5db909c12c45a19e5973dd9912e69e9a894e9b
erikalopes <201810099@aluno.fjn.edu.br>
2020-08-09 16:43:03 -0300

    history

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from "react";
+import { useHistory } from "react-router-dom";
 import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
@@ -9,6 +10,8 @@ import Select from "../../components/Select";
 import api from "../../services/api";
 
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
@@ -52,19 +55,24 @@ function TeacherForm() {
 
   function handleCreateClass(e: FormEvent) {
     e.preventDefault();
-    api.post("classes", {
-      name,
-      avatar,
-      whatsapp,
-      bio,
-      subject,
-      cost: Number(cost),
-      schedule: scheduleItems,
-    }).then(()=> {
-      alert('Cadastro realizado com sucesso!')
-    }).catch(()=> {
-      alert('Erro no cadastro')
-    });
+    api
+      .post("classes", {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost: Number(cost),
+        schedule: scheduleItems,
+      })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+
+        history.push("/");
+      })
+      .catch(() => {
+        alert("Erro no cadastro");
+      });
   }
 
   return (
