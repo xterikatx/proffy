a0a86f0590aadcfb2a0160f12fe39cd808edb888
erikalopes <201810099@aluno.fjn.edu.br>
2020-08-08 23:25:50 -0300

    add textrea component

--- web/src/pages/TeacherForm/index.js
+++ web/src/pages/TeacherForm/index.js
@@ -1,10 +1,45 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+import Input from "../../components/Input";
+import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from "../../components/Textarea";
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Curso da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} srt="Aviso importante" />
+            Importante <br />
+            Preencha todos os dados.
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   );
 }
